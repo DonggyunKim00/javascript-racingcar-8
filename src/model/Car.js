68e3cb7d4739e17c6b134d9bf417be4c9827c98c
donggyun <donggyunkim12@gmail.com>
2025-10-23 17:25:19 +0900

    refactor(Car): 캡슐화를 위한 name, distance private 선언 리팩토링

--- src/model/Car.js
+++ src/model/Car.js
@@ -1,18 +1,21 @@
 import { Random } from '@woowacourse/mission-utils';
 
 class Car {
+  #name;
+
+  #distance = 0;
+
   constructor(name) {
-    this.name = name;
-    this.distance = 0;
+    this.#name = name;
   }
 
   move() {
     const random = Random.pickNumberInRange(0, 9);
-    if (random >= 4) this.distance += 1;
+    if (random >= 4) this.#distance += 1;
   }
 
   getDistance() {
-    return this.distance;
+    return this.#distance;
   }
 }
 
