ff99274bbb0f7dbb3e80ddea441b428c1cc4de5f
donggyun <donggyunkim12@gmail.com>
2025-10-23 17:13:09 +0900

    feat(Car): 랜덤값에 대해 전진 기능 구현

---
+++ src/model/Car.js
@@ -0,0 +1,15 @@
+import { Random } from '@woowacourse/mission-utils';
+
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.distance = 0;
+  }
+
+  move() {
+    const random = Random.pickNumberInRange(0, 9);
+    if (random >= 4) this.distance += 1;
+  }
+}
+
+export default Car;
