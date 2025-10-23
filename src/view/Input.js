dbd319f3293a09ff60bf287ed692a4a6795ac55b
donggyun <donggyunkim12@gmail.com>
2025-10-23 16:19:15 +0900

    feat(view): 시도할 횟수 입력 기능 구현

--- src/view/Input.js
+++ src/view/Input.js
@@ -7,6 +7,12 @@ class Input {
     );
     return input;
   }
+
+  static async readRoundCount() {
+    const input =
+      await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+    return input;
+  }
 }
 
 export default Input;
