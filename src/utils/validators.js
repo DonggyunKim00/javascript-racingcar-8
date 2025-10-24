c81c5bb8c372e40a6a42abcaa3ac51f5d0274a9d
donggyun <donggyunkim12@gmail.com>
2025-10-24 15:29:14 +0900

    feat(validators): 자동차 이름 유효성 검증 함수 구현
    - isDuplicate(): 배열 내에서 중복된 문자열이 존재할 때 true 리턴
    - isValidCarNameLength(): 배열 내에서 문자열의 길이가 모두 1이상 5이하 일때 true 리턴

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,1 +1,9 @@
 export const isEmpty = (string) => !string.trim();
+
+export const isDuplicate = (arr) => {
+  const removedDuplicate = new Set(arr);
+  return removedDuplicate.size !== arr.length;
+};
+
+export const isValidCarNameLength = (arr) =>
+  !arr.some((value) => value.length < 1 || value.length > 5);
